54759eafa4cfe0161b6bcfb2406749d9eadbfa92
Bodige Varunsai <148956487+imvarun18@users.noreply.github.com>
2025-12-03 19:55:41 +0530

    updated

--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -9,16 +9,26 @@ public class User {
     private String userId;
     private String name;
     private String rollNumber;
+    private String email;
+    private String phone;
+    private String placementStatus;
+    private String company;
+    private String placementDate;
     
     // Default constructor
     public User() {
     }
     
     // Parametrized constructor
-    public User(String name, String rollNumber) {
+    public User(String name, String rollNumber, String email, String phone, String placementStatus, String company, String placementDate) {
         super();
         this.name = name;
         this.rollNumber = rollNumber;
+        this.email = email;
+        this.phone = phone;
+        this.placementStatus = placementStatus;
+        this.company = company;
+        this.placementDate = placementDate;
     }
     
     // Getters and Setters
@@ -44,6 +54,46 @@ public class User {
     
     public void setRollNumber(String rollNumber) {
         this.rollNumber = rollNumber;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getPhone() {
+        return phone;
+    }
+    
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    public String getPlacementStatus() {
+        return placementStatus;
+    }
+    
+    public void setPlacementStatus(String placementStatus) {
+        this.placementStatus = placementStatus;
+    }
+    
+    public String getCompany() {
+        return company;
+    }
+    
+    public void setCompany(String company) {
+        this.company = company;
+    }
+    
+    public String getPlacementDate() {
+        return placementDate;
+    }
+    
+    public void setPlacementDate(String placementDate) {
+        this.placementDate = placementDate;
     }
 }
 
